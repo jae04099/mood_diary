8135c84b840f8831b223fddc8e3e4ef65b397437
Adorevoy <47337588+jae04099@users.noreply.github.com>
2020-08-13 14:48:47 +0900

    음악 한줄효과, 한 칸에 여러개 들어가는 등 수정

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@
 
 //pink 버튼 클릭
 //메모지 색 변경
+
+$.ajax({
+    type: 'GET',
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + PostCode + '&api_key=48bee338cada7005923f7ef2dd843dfa&format=json',
+    success: function (response) {
+        let musicList = response["results"]["trackmatches"]["track"];
+        console.log(musicList[1])
+        for(let i = 0; i < musicList.length; i++){
+            let albumImg = musicList[i][]
+            let albumTitle =
+            let albumArtist =
+        }
+    }
+})
