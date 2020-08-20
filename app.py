3f4449188119d964654d6e788de2674ed10e20c7
Adorevoy <47337588+jae04099@users.noreply.github.com>
2020-08-20 21:21:46 +0900

    db기록을 프린트 함

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('localhost', 27017)
+db = client.dbmusicdiary
+
 
 @app.route('/login')
 def login():
@@ -23,6 +26,32 @@ def record():
 def checkrecord():
     return render_template('check-record.html')
 
+
+# API 역할 부분
+
+@app.route('/record', methods=['POST'])
+def write_record():
+    date_receive = request.form['date']
+    feel_receive = request.form['feel']
+    title_receive = request.form['musicTitle']
+    artist_receive = request.form['musicArtist']
+    diary_receive = request.form['diary']
+
+    music_diary = {
+        'date': date_receive,
+        'feel': feel_receive,
+        'music_title': title_receive,
+        'music_artist': artist_receive,
+        'diary': diary_receive
+    }
+
+    db.music_diary.insert_one(music_diary)
+    return jsonify({'result': 'success', 'msg': '저장 완료!'})
+
+@app.route('/checkrecorded', methods=['GET'])
+def read_record():
+    records = list(db.music_diary.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'records': records})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
