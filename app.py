4e2f6a3c858b37900ae6431a8999ec9543503e4a
Adorevoy <47337588+jae04099@users.noreply.github.com>
2020-08-27 23:00:29 +0900

    자잘한 수정, 쿠키 expires 적용 안되는 부분 수정

--- app.py
+++ app.py
@@ -5,11 +5,19 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbmusicdiary
 
-
 @app.route('/login')
+def loginwname():
+    return render_template('sample.html')
+
+
+@app.route('/showcookie')
+def showcookie():
+    return render_template('sample2.html')
+
+@app.route('/')
 def login():
     return render_template('login.html')
 
@@ -25,6 +33,30 @@ def record():
 @app.route('/checkrecord')
 def checkrecord():
     return render_template('check-record.html')
+
+@app.route('/satisfylist')
+def emotionsatisfy():
+    return render_template('musicByEmotionSatisfy.html')
+
+@app.route('/happylist')
+def emotionhappy():
+    return render_template('musicByEmotionHappy.html')
+
+@app.route('/lovelylist')
+def emotionlovely():
+    return render_template('musicByEmotionLovely.html')
+
+@app.route('/unsatisfylist')
+def emotionunsatisfy():
+    return render_template('musicByEmotionUnsatisfy.html')
+
+@app.route('/sadlist')
+def emotionsad():
+    return render_template('musicByEmotionSad.html')
+
+@app.route('/angrylist')
+def emotionangry():
+    return render_template('musicByEmotionAngry.html')
 
 
 # API 역할 부분
@@ -52,6 +84,14 @@ def write_record():
 def read_record():
     records = list(db.music_diary.find({}, {'_id': 0}))
     return jsonify({'result': 'success', 'records': records})
+
+##삭제 api
+@app.route('/checkrecord/delete', methods=['POST'])
+def delete_record():
+    title_receive = request.form.get('music_title')
+    print(title_receive)
+    db.music_diary.delete_one({'music_title': title_receive})
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
